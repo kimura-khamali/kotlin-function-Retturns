ef7f562be338dfc27803f3eeca6b2feb418ad8b6
kimura-khamali <kimurakhamali@gmail.com>
2024-02-23 07:49:12 +0300

    add changes

--- src/Main.kt
+++ src/Main.kt
@@ -3,6 +3,11 @@
 //Create and invoke a function that takes in a name and prints out “Hello ${name}” e.g given “Ada” it will print out “Hello Ada”  (2 points)
 /*fun main() {
     name(name: "Ada")
+      name: Array<Int>) {
+    nameOf(num1 =26,
+     num2 = 3)
+     sumAll(num1 = 1 , 2 ,  3 ,num4= 4)
+      interestingFact(set = "I love drawing and hiking")
 }
 fun name(name: String){
 
@@ -10,10 +15,7 @@ fun name(name: String){
 }*/
 //Create and invoke a function that given 2 numbers returns the remainder of their division (3 points)
 
-/*fun main(
-    name: Array<Int>) {
-    nameOf(num1 =26,
-     num2 = 3)
+/*
 }
 fun nameOf(num1: Int , num2: Int ): Int {
     var result = num1.rem(num2)
@@ -22,8 +24,7 @@ return (result)
 }*/
 
 //Create and invoke  function that returns the sum of any given 4 numbers (3 points)
-/*fun main():Unit {
-   sumAll(num1 = 1 , 2 ,  3 ,num4= 4)
+/*
 }
 fun sumAll(num1: Int ,num2: Int ,num3: Int ,num4: Int):Int {
     val sum = num1 + num2 + num3 + num4
@@ -32,9 +33,7 @@ fun sumAll(num1: Int ,num2: Int ,num3: Int ,num4: Int):Int {
 }*/
 
 //Create and invoke a function that prints out an interesting fact about yourself (2 points)
-/*fun main() {
- interestingFact(set = "I love drawing and hiking")
-}
+/*
 fun interestingFact(set: String) {
     println(set)
 }*/
