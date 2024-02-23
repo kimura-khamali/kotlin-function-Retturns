251663701de5f05cc131632bf5025cca0c26b4b6
kimura-khamali <kimurakhamali@gmail.com>
2024-02-23 08:34:51 +0300

    add file

--- src/Main.kt
+++ src/Main.kt
@@ -1,42 +1,33 @@
 
 //Assignment 2: Functions
-//Create and invoke a function that takes in a name and prints out “Hello ${name}” e.g given “Ada” it will print out “Hello Ada”  (2 points)
 fun main() {
     text(test = "Ada")
+    nameOf(num1 =26,
+     num2 = 3)
+     sumAll(num1 = 1 , 2 ,  3 ,num4= 4)
+     interestingFact(set = "I love drawing and hiking")
 }
+
 fun text(test: String){
 var text = "Hello ${test}"
     println(text)
 }
-//Create and invoke a function that given 2 numbers returns the remainder of their division (3 points)
 
-/*fun main(
-    name: Array<Int>) {
-    nameOf(num1 =26,
-     num2 = 3)
-}
 fun nameOf(num1: Int , num2: Int ): Int {
-    var result = num1.rem(num2)
+    var result = num1 %num2
     println(result)
 return (result)
-}*/
-
-//Create and invoke  function that returns the sum of any given 4 numbers (3 points)
-/*fun main():Unit {
-   sumAll(num1 = 1 , 2 ,  3 ,num4= 4)
 }
+
+
 fun sumAll(num1: Int ,num2: Int ,num3: Int ,num4: Int):Int {
     val sum = num1 + num2 + num3 + num4
     println(sum)
     return sum
-}*/
-
-//Create and invoke a function that prints out an interesting fact about yourself (2 points)
-/*fun main() {
- interestingFact(set = "I love drawing and hiking")
 }
+
 fun interestingFact(set: String) {
     println(set)
-}*/
+}
 
 
